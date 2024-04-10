ca1de8d338796e9bf12de42f3e8bb983094b4c8c
seungho <gomi090447@gmail.com>
2024-04-10 19:19:05 +0900

    feat: calendar apis

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const authMember = require("../middlewares/authmember");
 
+router.post("/add", authMember, async (req, res) => {});
 
+router.delete("/delete", authMember, async (req, res) => {});
 
+router.patch("/update", authMember, async (req, res) => {});
+
+router.get("/monthinfo", authMember, async (req, res) => {});
+
+router.get("/dayinfo", authMember, async (req, res) => {});
 
 
 
