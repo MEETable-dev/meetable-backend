c5734120935d222e6cb28d7ae82174a3f9642ad6
seungho <gomi090447@gmail.com>
2024-09-01 01:45:08 +0900

    hotfix: /promise/baseinfo 수정

--- routes/promise.js
+++ routes/promise.js
@@ -1344,9 +1344,9 @@ router.get("/baseinfo/:promiseid", authMember, async (req, res) => {
                 [req.memberId, promiseId]
             );
             promiseName =
-                promiseNameResult[0] === 0
+                promiseNameResult.length === 0
                     ? promise_name
-                    : promiseNameResult[0].member_promise_name;
+                    : promiseNameResult[0]?.member_promise_name;
         } else if (req.isMember === false) {
             promiseName = promise_name;
         }
